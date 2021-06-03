b2a28d4e4e0b33c46ba9affcc2fb20fdef2176f0
Jardeson Santos <JarDeVSon@github.com>
2021-06-03 00:36:53 -0300

    Criação do Page Object Model

--- src/test/java/Pages/CadastroPage.java
+++ src/test/java/Pages/CadastroPage.java
@@ -1,4 +1,36 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CadastroPage {
+    static WebDriver driver;
+
+
+    public CadastroPage(WebDriver driver){
+        CadastroPage.driver = driver;
+    }
+    public void preencherCadastro(){
+        WebElement nomeCompleto = driver.findElement(By.cssSelector("#nome"));
+        nomeCompleto.sendKeys("Trios");
+
+        WebElement telefone = driver.findElement(By.cssSelector("#telefone"));
+        telefone.sendKeys("(19)99999999");
+
+        WebElement email = driver.findElement(By.cssSelector("#email"));
+        email.sendKeys("triosdecapacitacao@gmail.com");
+
+        WebElement senha = driver.findElement(By.cssSelector("#senha"));
+        senha.sendKeys("pwd@123");
+
+        WebElement captcha = driver.findElement(By.cssSelector("input[maxlength='6']"));
+        captcha.sendKeys("KNGI3L");
+
+        WebElement entrar = driver.findElement(By.cssSelector("#btn_cadastro"));
+        entrar.click();
+
+    }
+
 }
--- src/test/java/Pages/LoginPage.java
+++ src/test/java/Pages/LoginPage.java
@@ -1,4 +1,51 @@
 package Pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class LoginPage {
+    static WebDriver driver;
+
+    public LoginPage(WebDriver driver){
+        LoginPage.driver = driver;
+    }
+
+    public void verificarOPreenchimentoDoEmail(){
+        WebElement email = driver.findElement(By.cssSelector("#email"));
+        email.sendKeys("Email");
+    }
+    public void verificarOPreenchimentoDaSenha(){
+        WebElement senha = driver.findElement(By.cssSelector("#senha"));
+        senha.sendKeys("Senha");
+    }
+    public void validarOLembreMeDaConta(){
+        WebElement lembreMe = driver.findElement(By.cssSelector("input[name='remember']"));
+        lembreMe.click();
+    }
+    public void validarOAcessoAContaNoSiteDaTrios(){
+        WebElement email = driver.findElement(By.cssSelector("#email"));
+        email.sendKeys("Email");
+        WebElement senha = driver.findElement(By.cssSelector("#senhha"));
+        senha.sendKeys("Senha");
+        WebElement entrar = driver.findElement(By.cssSelector("#btn_login"));
+        entrar.click();
+    }
+    public void validarORedirecionamentoDeEsqueceuSuaSenha(){
+        WebElement esqueceuSenha = driver.findElement(By.cssSelector("a[href='#lembrarsenha']"));
+        esqueceuSenha.click();
+        WebElement recuperarSenha = driver.findElement(By.cssSelector("#email-redefine-password"));
+        recuperarSenha.sendKeys("email@email.com");
+        WebElement btnEnviar = driver.findElement(By.cssSelector("#btnRedefinePassword"));
+        btnEnviar.click();
+    }
+    public void validarORedirecionamentoDeNaoConsegueSeAutenticar(){
+        WebElement seAutenticar = driver.findElement(By.cssSelector("a[href='#autenticacao']"));
+        seAutenticar.click();
+        WebElement recuperarSenha = driver.findElement(By.cssSelector("input[placeholder='Digite seu E-mail...']"));
+        recuperarSenha.sendKeys("email@email.com");
+        WebElement btnEnviar = driver.findElement(By.cssSelector("#btn_confirmacao"));
+        btnEnviar.click();
+    }
+
 }
