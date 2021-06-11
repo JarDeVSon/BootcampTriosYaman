087abf5775d8e7163736255a723c8ea28d82000f
Jardeson Santos <JarDeVSon@github.com>
2021-06-11 16:57:26 -0300

    Automação dos fluxos de Cadastro e Login

--- src/test/java/Aplicacao_WEB/Cadastro.java
+++ src/test/java/Aplicacao_WEB/Cadastro.java
@@ -7,19 +7,22 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Cadastro {
 
     static WebDriver driver;
     static CadastroPage cadastroPage;
-    static Object preencherCadastro;
 
     @Before
-    public void setDriver(){
+    public void setCadastroPage(){
         System.setProperty("webdriver.chrome.driver","C:\\Users\\jarde\\IdeaProjects\\Bootcamp-Turma1-SegEQuarta\\src\\test\\resources\\driver\\chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
         driver.get("https://triosdecapacitacao.eadplataforma.com/cadastro/");
 
-        cadastroPage = new CadastroPage(driver,preencherCadastro);
+        cadastroPage = new CadastroPage(driver);
     }
     @Test
     public void test(){
